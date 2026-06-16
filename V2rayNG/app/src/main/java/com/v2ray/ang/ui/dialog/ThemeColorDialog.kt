826718a8d16aec8d8a8e7a848d049f5aaf9a0257
kasanetetouwu0401 <kasanetetouwu0401@gmail.com>
2026-06-16 23:45:36 +0700

    Last Sync: 2026-06-16 23:45 (Mobile)

--- V2rayNG/app/src/main/java/com/v2ray/ang/ui/dialog/ThemeColorDialog.kt
+++ V2rayNG/app/src/main/java/com/v2ray/ang/ui/dialog/ThemeColorDialog.kt
@@ -42,7 +42,7 @@ class ThemeColorDialog : DialogFragment() {
     var onAppliedCallback: () -> Unit = {}
 
     override fun onStart() {
-        super.osStart()
+        super.onStart()
         WindowBlurUtils.applyWindowBlur(dialog?.window)
     }
 
@@ -67,7 +67,7 @@ class ThemeColorDialog : DialogFragment() {
             
             val styleRes = ThemeManager.getThemeStyleRes(key)
             val wrappedContext = ContextThemeWrapper(requireContext(), styleRes)
-            val m3PrimaryColor = wrappedContext.getColorAttr("colorPrimary")
+            val m3PrimaryColor = wrappedContext.getColorAttr(android.R.attr.colorPrimary)
 
             applyCircleDrawable(circle, m3PrimaryColor, isSelected)
             
@@ -96,7 +96,7 @@ class ThemeColorDialog : DialogFragment() {
                 .setContentBasedSource(rawCustomColor)
                 .build()
             val wrappedCustomContext = DynamicColors.wrapContextIfAvailable(requireContext(), customOptions)
-            val m3CustomPrimary = wrappedCustomContext.getColorAttr(android.R.attr.colorPrimary)
+            val m3CustomPrimary = wrappedCustomContext.getColorAttr("colorPrimary")
 
             applyCircleDrawable(customCircle, m3CustomPrimary, isCustomSelected)
 
