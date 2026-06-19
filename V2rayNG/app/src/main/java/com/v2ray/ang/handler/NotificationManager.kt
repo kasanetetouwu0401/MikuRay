916c0c60d9139090368bc49c4f459bf2c2f3a14a
kasanetetouwu0401 <kasanetetouwu0401@gmail.com>
2026-06-20 03:59:26 +0700

    Last Sync: 2026-06-20 03:59 (Mobile)

--- V2rayNG/app/src/main/java/com/v2ray/ang/handler/NotificationManager.kt
+++ V2rayNG/app/src/main/java/com/v2ray/ang/handler/NotificationManager.kt
@@ -32,6 +32,7 @@ object NotificationManager {
     private const val NOTIFICATION_PENDING_INTENT_STOP_V2RAY = 1
     private const val NOTIFICATION_PENDING_INTENT_RESTART_V2RAY = 2
     private const val NOTIFICATION_ICON_THRESHOLD = 3000
+    private const val QUERY_INTERVAL_MS = 1000L
 
     private var lastQueryTime = 0L
     private var connectStartTime = 0L
@@ -58,7 +59,7 @@ object NotificationManager {
         speedNotificationJob = CoroutineScope(Dispatchers.IO).launch {
             while (isActive) {
                 lastZeroSpeed = updateSpeedNotificationOnce(lastZeroSpeed)
-                delay(1000L)
+                delay(QUERY_INTERVAL_MS)
             }
         }
 
