15581db938270d28ed6d3a1dfdec959bc73db3a0
kasanetetouwu0401 <kasanetetouwu0401@gmail.com>
2026-07-01 02:40:00 +0700

    update dependencies

--- V2rayNG/app/src/main/java/com/neko/widget/ProfileBannerImageView.kt
+++ V2rayNG/app/src/main/java/com/neko/widget/ProfileBannerImageView.kt
@@ -123,7 +123,7 @@ class ProfileBannerImageView @JvmOverloads constructor(
                         .load(savedUri)
                         .diskCacheStrategy(DiskCacheStrategy.DATA)
                         .dontAnimate()
-                        .error(R.drawable.uwu_banner_profile)
+                        .error(R.drawable.uwu_banner_author)
                         .into(this)
                 } else {
                     loadDefault()
@@ -141,6 +141,6 @@ class ProfileBannerImageView @JvmOverloads constructor(
 
     private fun loadDefault() {
         Glide.with(this).clear(this)
-        setImageResource(R.drawable.uwu_banner_profile)
+        setImageResource(R.drawable.uwu_banner_author)
     }
 }
