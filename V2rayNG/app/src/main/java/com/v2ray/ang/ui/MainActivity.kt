865a8901032a85c134738790f019801c185d8aa1
kasanetetouwu0401 <kasanetetouwu0401@gmail.com>
2026-07-30 07:31:17 +0700

    update dependencies

--- V2rayNG/app/src/main/java/com/v2ray/ang/ui/MainActivity.kt
+++ V2rayNG/app/src/main/java/com/v2ray/ang/ui/MainActivity.kt
@@ -509,6 +509,7 @@ class MainActivity : HelperBaseActivity(),
             R.id.import_manually_trojan -> importManually(EConfigType.TROJAN.value)
             R.id.import_manually_wireguard -> importManually(EConfigType.WIREGUARD.value)
             R.id.import_manually_hysteria2 -> importManually(EConfigType.HYSTERIA2.value)
+            R.id.import_manually_ssh -> importManually(EConfigType.SSH.value)
         }
     }
 
