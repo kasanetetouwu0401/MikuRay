7a878306f89f96bbc694b4744bf40590dc4dca43
kasanetetouwu0401 <kasanetetouwu0401@gmail.com>
2026-09-09 05:00:49 +0700

    Last Sync: 2026-09-09 05:00 (Mobile)

--- MikuRay/app/src/main/java/com/miku/ray/core/LauncherManager.kt
+++ MikuRay/app/src/main/java/com/miku/ray/core/LauncherManager.kt
@@ -31,8 +31,14 @@ object LauncherManager {
         MmkvManager.encodeSettings(AppConfig.PREF_VPN_CONNECT_START_TIME, 0L)
     }
 
-    fun getConnectStartTime(): Long =
-    MmkvManager.decodeSettingsLong(AppConfig.PREF_VPN_CONNECT_START_TIME, 0L)
+    fun getConnectStartTime(): Long {
+        val startTime = MmkvManager.decodeSettingsLong(AppConfig.PREF_VPN_CONNECT_START_TIME, 0L)
+        if (startTime > 0L && !CoreServiceManager.isRunning()) {
+            markConnectStopped()
+            return 0L
+        }
+        return startTime
+    }
 
     fun isConnected(): Boolean = getConnectStartTime() > 0L
 
