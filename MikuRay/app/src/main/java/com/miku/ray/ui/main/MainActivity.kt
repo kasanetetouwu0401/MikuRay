f860058d70240b7e5ca48cfc61d9636a130a70c9
kasanetetouwu0401 <kasanetetouwu0401@gmail.com>
2026-09-09 06:38:37 +0700

    Last Sync: 2026-09-09 06:38 (Mobile)

--- MikuRay/app/src/main/java/com/miku/ray/ui/main/MainActivity.kt
+++ MikuRay/app/src/main/java/com/miku/ray/ui/main/MainActivity.kt
@@ -779,7 +779,6 @@ ShareConfigBottomSheet.OnShareOptionClickListener {
         binding.viewPager.apply {
             adapter = groupPagerAdapter
             isUserInputEnabled = true
-            offscreenPageLimit = 10
         }
     }
 
