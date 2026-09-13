d0662fb0e1484bd61b35f97bbafde993165e5717
kasanetetouwu0401 <kasanetetouwu0401@gmail.com>
2026-09-14 06:07:22 +0700

    improved

--- MikuRay/app/src/main/java/com/miku/ray/shapeimageview/Cookie4ImageView.kt
+++ MikuRay/app/src/main/java/com/miku/ray/shapeimageview/Cookie4ImageView.kt
@@ -14,6 +14,6 @@ class Cookie4ImageView @JvmOverloads constructor(
 ) : ShaderImageView(context, attrs, defStyle) {
 
     override fun createImageViewHelper(): ShaderHelper {
-        return SvgShader(R.raw.uwu_shape_cookie_9)
+        return SvgShader(R.raw.uwu_shape_cookie_4)
     }
 }
